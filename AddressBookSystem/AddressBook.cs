48cd8d80c105a84188996d8fc6230b374b6e45af
ak47 <kokitkarak@gmail.com>
2022-01-09 11:33:10 +0530

    ability to edit contacts

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -22,6 +22,40 @@ namespace AddressBookTrail
 
             }
         }
-    }
+
+        public void EditContact(string Name)
+        {
+            foreach (var contact in List)
+            {
+                if (contact.FirstName == Name)
+                {
+                    Console.WriteLine("Enter new First Name");
+                    string newFirstName = Console.ReadLine();
+                    contact.FirstName = newFirstName;
+                    Console.WriteLine("Enter new Last Name:");
+                    string newLastName = Console.ReadLine();
+                    contact.LastName = newLastName;
+                    Console.WriteLine("Enter new Address:");
+                    string newAddress = Console.ReadLine();
+                    contact.Address = newAddress;
+                    Console.WriteLine("Enter new City:");
+                    string newCity = Console.ReadLine();
+                    contact.City = newCity;
+                    Console.WriteLine("Enter new State:");
+                    string newState = Console.ReadLine();
+                    contact.State = newState;
+                    Console.WriteLine("Enter new Zip Code:");
+                    string newZipcode = Console.ReadLine();
+                    contact.ZipCode = newZipcode;
+                    Console.WriteLine("Enter new Phone Number:");
+                    string newPhonenumber = Console.ReadLine();
+                    contact.PhoneNumber = newPhonenumber;
+                    Console.WriteLine("Enter new Email:");
+                    string newEmail = Console.ReadLine();
+                    contact.Email = newEmail;
+                    Console.WriteLine("--------------------------------------------");
+                }
+            }
+        }
 
 }
